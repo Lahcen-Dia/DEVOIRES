0937d4ca25fe1995f4ff060e9b9f3978692837f4
Lahcen&Dia <62522174+Lahcen-Dia@users.noreply.github.com>
2020-04-04 02:17:32 +0100

    Add files via upload

---
+++ devoir2/valideDate.php
@@ -0,0 +1,36 @@
+
+<?php 
+echo"<h1>Validation de la date</h1>";
+$date = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{   
+   
+            
+              $jour = $_POST["jour"];
+              $mois = $_POST["mois"];
+              $annee = $_POST["annee"];
+              //$date=date_parse_from_format("d/m/Y", $date);
+              //$date = date_parse($date);
+              if(!(checkdate($jour, $mois, $annee))){
+               echo "la date saisie ";
+               echo $_POST["jour"];
+               echo"/";
+               echo $_POST["mois"];
+               echo"/";
+               echo $_POST["annee"];
+               echo "<br>";
+               echo"invalide  date";
+
+               } else {
+                 echo "la date saisie ";
+               echo $_POST["jour"];
+               echo"/";
+               echo $_POST["mois"];
+               echo"/";
+               echo $_POST["annee"];
+               echo "<br>";
+                echo "date saisie est valide";
+            } 
+}
+?>   
